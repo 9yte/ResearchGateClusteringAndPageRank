86fca486f236e6a0da41caf16ab43c8002232c8c
Milad Asgari <merlion380@gmail.com>
2016-01-28 09:40:50 +0330

    elastic completed

--- elasticSearch.py
+++ elasticSearch.py
@@ -3,23 +3,76 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch([{u'host': u'127.0.0.1', u'port': 9200}])
 
-doc = {
+doc1 = {
     'author': 'kimchy',
     'text': 'Elasticsearch: cool. bonsai cool.',
     'timestamp': datetime.now()
 }
+doc2 = {
+    'author': 'kimchy2',
+    'text': 'Elasticsearch2: cool. bonsai cool.',
+    'timestamp': datetime.now()
+}
 
-res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
-print(res['created'])
+docs = [doc1, doc2]
 
-res = es.get(index="test-index", doc_type='tweet', id=1)
-print(res['_source'])
+# es.indices.create(index="researchgate-data",
+#                   body={
+#                          "mappings": {
+#                              "page": {
+#                                  "_source": { "enabled": True },
+#                                  "properties": {
+#                                      "url": {
+#                                          "type": "string"
+#                                      },
+#                                      "page_text": {
+#                                          "type": "string",
+#                                          "term_vector": "yes"
+#                                      },
+#                                      "title": {
+#                                          "type": "string",
+#                                          "term_vector": "yes"
+#                                      }
+#                                  }
+#                              }
+#                          }
+#                   })
+#
+# index_id = 0
+# # try:
+# for doc in docs:
+#     es.index(index="researchgate-data", doc_type="paper", body=doc, id=index_id)
+#     index_id += 1
+# except:
+#     print("exception")
 
-es.indices.refresh(index="test-index")
-#termVector
+index_id = 0
+vectors = []
+for doc in docs:
+    temp = es.termvectors(index="researchgate-data",  doc_type="paper", id=index_id,  term_statistics=False, field_statistics=False, fields=['text'])
+    index_id += 1
+    vec = temp["term_vectors"]["text"]["terms"]
+    vec = dict(vec)
+    vec_clean = {}
+    for k, v in vec.items():
+        vec_clean[k] = v['term_freq']
+    vectors.append(vec_clean)
 
-res = es.search(index="test-index", body={"query": {"match_all": {}}})
+# es.indices.delete(index="researchgate-data")
 
-print("Got %d Hits:" % res['hits']['total'])
-for hit in res['hits']['hits']:
-    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+# b=dict([(k, v['term_freq'])for k,v in temp.iteritems()])
+# -----------------------------------
+# res = es.index(index="researchgate-data", doc_type='paper', id=1, body=doc)
+# print(res['created'])
+#
+# res = es.get(index="researchgate-data", doc_type='paper', id=1)
+# print(res['_source'])
+#
+# es.indices.refresh(index="researchgate-data")
+# #termVector
+#
+# res = es.search(index="researchgate-data", body={"query": {"match_all": {}}})
+#
+# print("Got %d Hits:" % res['hits']['total'])
+# for hit in res['hits']['hits']:
+#     print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
